783f7ee51867369312490d748d9b7e962e89450a
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-07-10 12:30:09 +0200

    Docs: updated DefaultPage

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
-<!-- eslint-disable prettier/prettier -->
 <template>
-	<nav>
-		<router-link to="/about">About us</router-link>
-	</nav>
-	<router-view />
+  <nav>
+    <router-link to="/about">About us</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<!-- eslint-disable prettier/prettier -->
 <style>
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-	}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-	nav {
-		padding: 30px;
-	}
+nav {
+  padding: 30px;
+}
 
-	nav a {
-		font-weight: bold;
-		color: #2c3e50;
-	}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-	nav a.router-link-exact-active {
-		color: #42b983;
-	}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/components/Default.vue
+++ src/components/Default.vue
@@ -1,230 +1,218 @@
-<!-- eslint-disable prettier/prettier -->
-
 <template>
-	<div>
-		<header>
-			<h1>{{ HeaderText() }}</h1>
-		</header>
-		<main>
-			<h2>{{ MainHeaderText() }}</h2>
-			<p>{{ MainText() }}</p>
-		</main>
+  <div>
+    <header>
+      <h1>{{ HeaderText() }}</h1>
+    </header>
+    <main>
+      <h2>{{ MainHeaderText() }}</h2>
+      <p>{{ MainText() }}</p>
+    </main>
 
-		<h2>{{ Heading2Text() }}</h2>
-		<h3>{{ Heading3Text() }}</h3>
+    <h2>{{ Heading2Text() }}</h2>
+    <h3>{{ Heading3Text() }}</h3>
 
-		<input
-			type="text"
-			:placeholder="TextInputPlaceholder('Text')"
-		/>
+    <input type="text" :placeholder="TextInputPlaceholder('Text')" />
 
-		<br />
+    <br />
 
-		<input
-			type="email"
-			:placeholder="TextInputPlaceholder('Email')"
-		/>
+    <input type="email" :placeholder="TextInputPlaceholder('Email')" />
 
-		<br />
+    <br />
 
-		<select class="optionsItem">
-			<option
-				v-for="option in optionsItem"
-				:value="option.value"
-				:key="option.value"
-			>
-				{{ option.label }}
-			</option>
-		</select>
+    <select class="optionsItem">
+      <option
+        v-for="option in optionsItem"
+        :value="option.value"
+        :key="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
 
-		<select class="optionsColor">
-			<option
-				v-for="option in optionsColor"
-				:value="option.value"
-				:key="option.value"
-			>
-				{{ option.label }}
-			</option>
-		</select>
+    <select class="optionsColor">
+      <option
+        v-for="option in optionsColor"
+        :value="option.value"
+        :key="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
 
-		<select class="optionsMaterial">
-			<option
-				v-for="option in optionsMaterial"
-				:value="option.value"
-				:key="option.value"
-			>
-				{{ option.label }}
-			</option>
-		</select>
+    <select class="optionsMaterial">
+      <option
+        v-for="option in optionsMaterial"
+        :value="option.value"
+        :key="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
 
-		<br />
+    <br />
 
-		<div class="date">
-			<time datetime="">Placeholder Select Datum</time>
-		</div>
+    <div class="date">
+      <time datetime="">Placeholder Select Datum</time>
+    </div>
 
-		<div class="location">Placeholder Select Location</div>
+    <div class="location">Placeholder Select Location</div>
 
-		<ul>
-			<li
-				v-for="item in listItems"
-				:key="item"
-			>
-				{{ item }}
-			</li>
-		</ul>
-		<div class="backNext">
-			<button>{{ ButtonBack() }}</button>
-			<button>{{ ButtonNext() }}</button>
-		</div>
+    <ul>
+      <li v-for="item in listItems" :key="item">
+        {{ item }}
+      </li>
+    </ul>
 
-		<br />
+    <div class="backNext">
+      <button>{{ ButtonBack() }}</button>
+      <button>{{ ButtonNext() }}</button>
+    </div>
 
-		<div class="lostFound">
-			<Button>{{ ButtonLost() }}</Button>
-			<Button>{{ ButtonFound() }}</Button>
-		</div>
-		<footer>
-			<p>{{ FooterText() }}</p>
-		</footer>
-	</div>
-	<div class="counter">
-		Placeholder für Counter
-		<p>{{ counterValue }}</p>
-	</div>
+    <br />
+
+    <div class="lostFound">
+      <Button>{{ ButtonLost() }}</Button>
+      <Button>{{ ButtonFound() }}</Button>
+    </div>
+    <footer>
+      <p>{{ FooterText() }}</p>
+    </footer>
+  </div>
+  <div class="counter">
+    Placeholder für Counter
+    <p>{{ counterValue }}</p>
+  </div>
 </template>
 
-<!-- eslint-disable prettier/prettier -->
 <script>
-	export default {
-		name: "DefaultComponent",
-		data() {
-			return {
-				optionsItem: [
-					{ value: "product", label: "Wallet" },
-					{ value: "product", label: "Bag" },
-					{ value: "product", label: "Phone" },
-					{ value: "product", label: "Watch" },
-					{ value: "product", label: "Key" },
-					{ value: "product", label: "Clothing" },
-				],
+export default {
+  name: "DefaultComponent",
+  data() {
+    return {
+      optionsItem: [
+        { value: "product", label: "Wallet" },
+        { value: "product", label: "Bag" },
+        { value: "product", label: "Phone" },
+        { value: "product", label: "Watch" },
+        { value: "product", label: "Key" },
+        { value: "product", label: "Clothing" },
+      ],
 
-				optionsColor: [
-					{ value: "color", label: "Black" },
-					{ value: "color", label: "White" },
-					{ value: "color", label: "Brown" },
-					{ value: "color", label: "Red" },
-					{ value: "color", label: "Green" },
-					{ value: "color", label: "Blue" },
-					{ value: "color", label: "Yellow" },
-					{ value: "color", label: "Silver" },
-					{ value: "color", label: "Gold" },
-				],
+      optionsColor: [
+        { value: "color", label: "Black" },
+        { value: "color", label: "White" },
+        { value: "color", label: "Brown" },
+        { value: "color", label: "Red" },
+        { value: "color", label: "Green" },
+        { value: "color", label: "Blue" },
+        { value: "color", label: "Yellow" },
+        { value: "color", label: "Silver" },
+        { value: "color", label: "Gold" },
+      ],
 
-				optionsMaterial: [
-					{ value: "material", label: "Kind of Leather" },
-					{ value: "material", label: "Kind of Plastic" },
-					{ value: "material", label: "Kind of Metal" },
-					{ value: "material", label: "Kind of Wood" },
-					{ value: "material", label: "Kind of textiles" },
-				],
+      optionsMaterial: [
+        { value: "material", label: "Kind of Leather" },
+        { value: "material", label: "Kind of Plastic" },
+        { value: "material", label: "Kind of Metal" },
+        { value: "material", label: "Kind of Wood" },
+        { value: "material", label: "Kind of textiles" },
+      ],
 
-				listItems: [
-					"Placeholder für Item-Liste oder Item nachdem User-Auswahl getroffen wurde",
-				],
-			};
-		},
-		methods: {
-			HeaderText() {
-				return "LOST & FOUND";
-			},
-			MainHeaderText() {
-				return "Main Content";
-			},
-			MainText() {
-				return "Some text here...";
-			},
-			FooterText() {
-				return "Footer content";
-			},
-			Heading1Text() {
-				return "H1 Heading";
-			},
-			Heading2Text() {
-				return "H2 Heading";
-			},
-			Heading3Text() {
-				return "H3 Heading";
-			},
-			ButtonBack() {
-				return "Back";
-			},
-			ButtonNext() {
-				return "Next";
-			},
-			ButtonLost() {
-				return "Lost";
-			},
-			ButtonFound() {
-				return "Found";
-			},
-			TextInputPlaceholder(type) {
-				return `${type} input`;
-			},
-		},
-	};
+      listItems: [
+        "Placeholder für Item-Liste oder Item nachdem User-Auswahl getroffen wurde",
+      ],
+    };
+  },
+  methods: {
+    HeaderText() {
+      return "LOST & FOUND";
+    },
+    MainHeaderText() {
+      return "Main Content";
+    },
+    MainText() {
+      return "Some text here...";
+    },
+    FooterText() {
+      return "Footer content";
+    },
+    Heading1Text() {
+      return "H1 Heading";
+    },
+    Heading2Text() {
+      return "H2 Heading";
+    },
+    Heading3Text() {
+      return "H3 Heading";
+    },
+    ButtonBack() {
+      return "Back";
+    },
+    ButtonNext() {
+      return "Next";
+    },
+    ButtonLost() {
+      return "Lost";
+    },
+    ButtonFound() {
+      return "Found";
+    },
+    TextInputPlaceholder(type) {
+      return `${type} input`;
+    },
+  },
+};
 </script>
 
-<!-- eslint-disable prettier/prettier -->
 <style>
-	/* SCOPED ? */
-	header {
-		background-color: #f2f2f2;
-		padding: 20px;
-	}
+/* SCOPED ? */
+header {
+  background-color: #f2f2f2;
+  padding: 20px;
+}
 
-	main {
-		padding: 20px;
-	}
+main {
+  padding: 20px;
+}
 
-	footer {
-		background-color: #f2f2f2;
-		padding: 20px;
-	}
+footer {
+  background-color: #f2f2f2;
+  padding: 20px;
+}
 
-	h1 {
-		color: #333;
-	}
+h1 {
+  color: #333;
+}
 
-	h2 {
-		color: #555;
-	}
+h2 {
+  color: #555;
+}
 
-	h3 {
-		color: #777;
-	}
+h3 {
+  color: #777;
+}
 
-	button {
-		background-color: #4caf50;
-		color: white;
-		padding: 10px 20px;
-		margin: 10px;
-		border: none;
-		cursor: pointer;
-	}
+button {
+  background-color: #4caf50;
+  color: white;
+  padding: 10px 20px;
+  margin: 10px;
+  border: none;
+  cursor: pointer;
+}
 
-	input,
-	select {
-		margin: 10px 0;
-		padding: 10px;
-	}
+input,
+select {
+  margin: 10px 0;
+  padding: 10px;
+}
 
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-	li {
-		margin-bottom: 10px;
-	}
+li {
+  margin-bottom: 10px;
+}
 </style>
