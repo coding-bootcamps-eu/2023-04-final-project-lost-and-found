648b1f287467b7f9b906da3128b74fcf045217f5
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-07-12 11:42:46 +0200

    docs: Grundgerüst gebaut

---
+++ src/views/LostMatchPage.vue
@@ -0,0 +1,118 @@
+<template>
+  <header>
+    <h1>{{ HeaderLostText() }}</h1>
+  </header>
+
+  <main>
+    <h2>YES!</h2>
+    <p>
+      We already have some matches for you! Is one of these items yours? (click
+      it and get connected with the founder)
+    </p>
+    <ul>
+      <li v-for="item in listItems" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+  </main>
+  <footer>
+    <nav>
+      <router-link to="/home">Home</router-link>
+    </nav>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "DefaultComponent",
+  data() {
+    return {
+      optionsItem: [
+        { value: "product", label: "Wallet" },
+        { value: "product", label: "Bag" },
+        { value: "product", label: "Phone" },
+        { value: "product", label: "Watch" },
+        { value: "product", label: "Key" },
+        { value: "product", label: "Clothing" },
+      ],
+
+      optionsColor: [
+        { value: "color", label: "Black" },
+        { value: "color", label: "White" },
+        { value: "color", label: "Brown" },
+        { value: "color", label: "Red" },
+        { value: "color", label: "Green" },
+        { value: "color", label: "Blue" },
+        { value: "color", label: "Yellow" },
+        { value: "color", label: "Silver" },
+        { value: "color", label: "Gold" },
+      ],
+
+      optionsMaterial: [
+        { value: "material", label: "Kind of Leather" },
+        { value: "material", label: "Kind of Plastic" },
+        { value: "material", label: "Kind of Metal" },
+        { value: "material", label: "Kind of Wood" },
+        { value: "material", label: "Kind of textiles" },
+      ],
+
+      listItems: [
+        "Placeholder für Item-Liste oder Item nachdem User-Auswahl getroffen wurde",
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* SCOPED ? */
+header {
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+main {
+  padding: 20px;
+}
+
+footer {
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+}
+
+h2 {
+  color: #555;
+}
+
+h3 {
+  color: #777;
+}
+
+button {
+  background-color: #4caf50;
+  color: white;
+  padding: 10px 20px;
+  margin: 10px;
+  border: none;
+  cursor: pointer;
+}
+
+input,
+select {
+  margin: 10px 0;
+  padding: 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+</style>
