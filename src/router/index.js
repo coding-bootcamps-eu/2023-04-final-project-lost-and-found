4bcaef4a2d144b84514e35721dfb66f195e593e9
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-07-13 07:46:21 +0200

    code: fixed merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/LandingPage.vue";
-
+import LostPage from "../views/LostPage.vue";
+import FoundPage from "../views/FoundPage.vue";
 import LostMatchPage from "../views/LostMatchPage.vue";
-
 import FoundMessagePage from "../views/FoundMessagePage.vue";
-
-import LostnonMatchPage from "../views/LostnonMatchPage.vue";
-
-import FoundMessagePage from "../views/FoundMessagePage.vue";
-import LostMatchPage from "../views/LostMatchPage.vue";
 import LostnonMatchPage from "../views/LostnonMatchPage.vue";
 import AboutPage from "../views/AboutPage.vue";
 import ContactPage from "../views/ContactPage.vue";
 import ImpressumPage from "../views/ImpressumPage.vue";
-import LostPage from "../views/LostPage.vue";
-import FoundPage from "../views/FoundPage.vue";
-
-
 
 const routes = [
   {
@@ -25,28 +16,21 @@ const routes = [
     component: HomeView,
   },
   {
-
     path: "/found/foundmessagepage",
     name: "foundmessage",
     component: FoundMessagePage,
   },
   {
-
-
     path: "/lost/lostmatchpage",
     name: "lostmatch",
     component: LostMatchPage,
   },
   {
-
-
     path: "/lost/lostnonmatchpage",
     name: "lostnonmatch",
     component: LostnonMatchPage,
   },
   {
-
-
     path: "/about",
     name: "about",
     component: AboutPage,
