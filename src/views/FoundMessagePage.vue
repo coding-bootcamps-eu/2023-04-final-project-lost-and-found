8f41fc21c7765b378d313accd0c7d008c1bb95c4
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-07-18 22:34:18 +0200

    docs: update FoundMessagePage

--- src/views/FoundMessagePage.vue
+++ src/views/FoundMessagePage.vue
@@ -1,21 +1,26 @@
 <template>
   <header>
-    <h1>Test</h1>
+    <h1 class="found">FOUND</h1>
   </header>
 
   <main class="text">
     <h2>
-      Thank you for the advertisement! You will make someone very happy again!
+      Thank you for the advertisement,
+      <br />
+      you will make someone very happy again!
     </h2>
-    <p>As soon as someone is looking for his item, we will let you know.</p>
+    <p>
+      As soon as someone is looking for his item, we will let you know via
+      emailing you.
+    </p>
   </main>
   <footer>
     <nav>
-      <router-link to="/FoundPage">Back</router-link>
+      <router-link to="/FoundPage" class="backLink">Back</router-link>
     </nav>
     <br />
     <nav>
-      <router-link to="/">Home</router-link>
+      <router-link to="/" class="homeLink">Home</router-link>
     </nav>
   </footer>
 </template>
@@ -23,49 +28,59 @@
 <script></script>
 
 <style scoped>
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+/* SCOPED ? */
+html {
+  font-family: "Roboto", sans-serif;
 }
 
-.logo img {
-  width: 100%;
-  height: auto;
-}
-
-.mainContent {
+header {
   display: flex;
+  flex-direction: row;
   justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
+  background-color: #f5f1f1;
 }
 
-footer {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  flex-wrap: wrap;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  height: 50px;
-  background-color: #f2f2f2;
-  padding: 20px;
+.found {
+  color: #a6b8fc;
 }
 
 h1 {
-  color: #333;
+  font-size: 50px;
 }
 
-button {
-  background-color: #4caf50;
-  color: white;
-  padding: 10px 20px;
-  margin: 10px;
-  border: none;
-  cursor: pointer;
+main {
+  text-align: center;
+  background-color: #f5f1f1;
+  padding: 20px;
+}
+
+h2 {
+  padding-bottom: 50px;
+  color: #4c3d40;
+}
+
+p {
+  color: #a6b8fc;
+  padding-top: 30px;
+  margin-bottom: 0;
+  font-size: 20px;
+  padding-bottom: 50px;
+}
+
+footer {
+  height: 30px;
+  background-color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.backLink {
+  color: #4c3d40;
+}
+
+.homeLink {
+  color: #4c3d40;
 }
 </style>
